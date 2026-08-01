e82475402354e7ac4214e95cd166a20bfe01597a
FrozenKangaroo <foristick@gmail.com>
2026-08-01 23:37:04 +1000

    Wire v0.15.1 Collaborator budget window

---
+++ scripts/ui/workspace_v0151.gd
@@ -0,0 +1,17 @@
+class_name CCFWorkspaceV0151View
+extends "res://scripts/ui/workspace_v015.gd"
+
+const CHARACTER_COLLABORATOR_WINDOW_V0151 = preload("res://scripts/ui/character_collaborator_window_v0151.gd")
+
+
+func _build_character_collaborator_window_v015() -> void:
+	_character_collaborator_window = CHARACTER_COLLABORATOR_WINDOW_V0151.new()
+	_character_collaborator_window.visible = false
+	_character_collaborator_window.force_native = true
+	_character_collaborator_window.transient = false
+	_character_collaborator_window.exclusive = false
+	_character_collaborator_window.set_generation_service(_generation_service)
+	_character_collaborator_window.sessions_changed.connect(_on_collaborator_sessions_changed_v015)
+	_character_collaborator_window.character_draft_ready.connect(_on_collaborator_character_draft_ready_v015)
+	add_child(_character_collaborator_window)
+	_character_collaborator_window.hide()
